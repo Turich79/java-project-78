4f8860755bf9120a14420bc24ff81076b604565e
Artur Neustroev <turich79@mail.ru>
2024-08-13 22:16:47 +0700

    1_2 step

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     application
     checkstyle
+    jacoco
 }
 
 group = "io.hexlet"
@@ -18,8 +19,16 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+    implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.4")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
